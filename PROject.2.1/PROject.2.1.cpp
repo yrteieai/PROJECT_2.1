9ce60e95e2b63688130acf3893d826c5437206d6
Anya Stefurak <anyutastefurak@gmail.com>
2024-09-17 00:27:57 +0300

    first branch_02 commit

--- PROject.2.1/PROject.2.1.cpp
+++ PROject.2.1/PROject.2.1.cpp
@@ -12,17 +12,17 @@ using namespace std;
 int main()
 {
     double a;
-    double z1; 
-    // double z2;
+    // double z1; 
+    double z2;
 
     cout << "a=?";
     cin >> a;
 
-    z1 = (cos(a) + sin(a)) / (cos(a) - sin(a));
-    // z2 = tan(2 * a) + 1 / cos(2 * a); // sec(x) = 1 / cos(x)
+    // z1 = (cos(a) + sin(a)) / (cos(a) - sin(a));
+    z2 = tan(2 * a) + 1 / cos(2 * a); // sec(x) = 1 / cos(x)
 
-    cout << "z1 = " << z1 << endl;
-    // cout << "z2 = " << z2 << endl;
+    //cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
 
     cin.get();
     return 0;
